b8618f76c1c44c09bffbea89dfcc7c2f37e46444
Andrew Barba <barba@hey.com>
2021-11-01 12:50:14 -0400

    Match js names

--- gettyimages-api.d.ts
+++ gettyimages-api.d.ts
@@ -1,19 +1,26 @@
 declare module 'gettyimages-api' {
-  export interface GettyCustomRequest {
+  export interface GettyApiRequest {
+    hostName: string
+  }
+
+  export interface CustomRequest extends GettyApiRequest {
     execute(): Promise<any>
     withRoute(route: string): this
     withMethod(method: string): this
+    withBody(body: any): this
     withQueryParameters(params: { [key: string]: string }): this
   }
-  class Getty {
+  
+  class GettyImagesApi {
     constructor(props: {
-      apiKey?: string
-      apiSecret?: string
+      apiKey: string
+      apiSecret: string
       username?: string
       password?: string
-    })
-    customrequest(): GettyCustomRequest
+    }, hostName?: string)
+    customrequest(): CustomRequest
     getAccessToken(): Promise<{ access_token: string }>
   }
-  export default Getty
+
+  export default GettyImagesApi
 }
