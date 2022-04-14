2d567f6530b8c8e5b92f2847909fcfaea7bd0450
Mark Pitman <mapitman@users.noreply.github.com>
2022-04-14 16:26:08 -0700

    Add ESLint (#40)
    * Add ESLint
    
    Fix all ESLint errors
    Add job to run linter against pull requests

--- tests/authtests.js
+++ tests/authtests.js
@@ -2,47 +2,44 @@ const api = require("../gettyimages-api");
 const nock = require("nock");
 const test = require("ava");
 
-test.beforeEach(t=>{
+test.beforeEach(() => {
     nock("https://api.gettyimages.com")
-                .post("/oauth2/token", "client_id=apikey&client_secret=apisecret&grant_type=client_credentials")
-                .reply(200, {
-                    access_token: "client_credentials_access_token",
-                    token_type: "Bearer",
-                    expires_in: "1800"
-                })
-                .post("/oauth2/token", "client_id=apikey&client_secret=apisecret&grant_type=password&username=username&password=password")
-                .reply(200, {
-                access_token: "resource_owner_access_token",
-                token_type: "Bearer",
-                expires_in: "1800",
-                refresh_token: "refreshtoken"
-                })
-                .post("/oauth2/token", "client_id=apikey&client_secret=apisecret&refresh_token=refreshtoken&grant_type=refresh_token")
-                .reply(200, {
-                    access_token: "accesstoken",
-                    token_type: "Bearer",
-                    expires_in: "1800"
-                });
+        .post("/oauth2/token", "client_id=apikey&client_secret=apisecret&grant_type=client_credentials")
+        .reply(200, {
+            access_token: "client_credentials_access_token",
+            token_type: "Bearer",
+            expires_in: "1800"
+        })
+        .post("/oauth2/token", "client_id=apikey&client_secret=apisecret&grant_type=password&username=username&password=password")
+        .reply(200, {
+            access_token: "resource_owner_access_token",
+            token_type: "Bearer",
+            expires_in: "1800",
+            refresh_token: "refreshtoken"
+        })
+        .post("/oauth2/token", "client_id=apikey&client_secret=apisecret&refresh_token=refreshtoken&grant_type=refresh_token")
+        .reply(200, {
+            access_token: "accesstoken",
+            token_type: "Bearer",
+            expires_in: "1800"
+        });
 });
 
-test("Client Credentials should return an access token when an api key and secret are provided", t => {  
+test("Client Credentials should return an access token when an api key and secret are provided", async t => {  
     var client = new api({ apiKey: "apikey", apiSecret: "apisecret" }, null);
-    return Promise.resolve(client.getAccessToken()).then(res => {
-        t.is(res.access_token, "client_credentials_access_token");
-    });
+    const res = await client.getAccessToken();
+    t.is(res.access_token, "client_credentials_access_token");
 });
 
-test("Resource Owner Grant should return an access token when an api key, secret, username, and password are provided", t => {  
+test("Resource Owner Grant should return an access token when an api key, secret, username, and password are provided", async t => {  
     var client = new api({ apiKey: "apikey", apiSecret: "apisecret", username: "username", password: "password" }, null);
-    return Promise.resolve(client.getAccessToken()).then(res => {
-        t.is(res.access_token, "resource_owner_access_token");
-    });
+    const res = await client.getAccessToken();
+    t.is(res.access_token, "resource_owner_access_token");
 });
 
-test("Refresh Token should return an access token when an api key, secret, and refresh token are provided", t => {  
+test("Refresh Token should return an access token when an api key, secret, and refresh token are provided", async t => {  
     var client = new api({ apiKey: "apikey", apiSecret: "apisecret", refresh_token:"refreshtoken" }, null);
-    return Promise.resolve(client.getAccessToken()).then(res => {
-        t.is(res.access_token, "accesstoken");
-    });
+    const res = await client.getAccessToken();
+    t.is(res.access_token, "accesstoken");
 });
 
--- tests/downloadsimagestests.js
+++ tests/downloadsimagestests.js
@@ -2,63 +2,58 @@ const api = require("../gettyimages-api");
 const nock = require("nock");
 const test = require("ava");
 
-test.beforeEach(t=>{
+test.beforeEach(() => {
     nock("https://api.gettyimages.com")
-            .post("/oauth2/token", "client_id=apikey&client_secret=apisecret&grant_type=client_credentials")
-            .reply(200, {
-                access_token: "client_credentials_access_token",
-                token_type: "Bearer",
-                expires_in: "1800"
-            })
-            .post("/v3/downloads/images/123")
-            .query({ "auto_download": "false" })
-            .reply(200, {response : "response"})
-            .post("/v3/downloads/images/123")
-            .query({ "file_type": "jpg", "auto_download": "false" })
-            .reply(200, {response : "response"})
-            .post("/v3/downloads/images/123")
-            .query({ "height": "592", "auto_download": "false"})
-            .reply(200, {response : "response"})
-            .post("/v3/downloads/images/123")
-            .query({ "product_id": 5678, "auto_download": "false" })
-            .reply(200, {response : "response"})
-            .post("/v3/downloads/images/123")
-            .query({ "product_type": "easyaccess", "auto_download": "false" })
-            .reply(200, {response : "response"});
+        .post("/oauth2/token", "client_id=apikey&client_secret=apisecret&grant_type=client_credentials")
+        .reply(200, {
+            access_token: "client_credentials_access_token",
+            token_type: "Bearer",
+            expires_in: "1800"
+        })
+        .post("/v3/downloads/images/123")
+        .query({ "auto_download": "false" })
+        .reply(200, {response : "response"})
+        .post("/v3/downloads/images/123")
+        .query({ "file_type": "jpg", "auto_download": "false" })
+        .reply(200, {response : "response"})
+        .post("/v3/downloads/images/123")
+        .query({ "height": "592", "auto_download": "false"})
+        .reply(200, {response : "response"})
+        .post("/v3/downloads/images/123")
+        .query({ "product_id": 5678, "auto_download": "false" })
+        .reply(200, {response : "response"})
+        .post("/v3/downloads/images/123")
+        .query({ "product_type": "easyaccess", "auto_download": "false" })
+        .reply(200, {response : "response"});
 });
 
-test("DownloadsImages: When given an id, the id will be part of the path", t => {  
+test("DownloadsImages: When given an id, the id will be part of the path", async t => {  
     var client = new api({ apiKey: "apikey", apiSecret: "apisecret" }, null);
-    return Promise.resolve(client.downloadsimages().withId("123").execute()).then(res => {
-        t.is(res.response, "response");
-    });
+    const res = await client.downloadsimages().withId("123").execute();
+    t.is(res.response, "response");
 });
 
-test("DownloadsImages: When given a file type, the file_type will be part of the query", t => {  
+test("DownloadsImages: When given a file type, the file_type will be part of the query", async t => {  
     var client = new api({ apiKey: "apikey", apiSecret: "apisecret" }, null);
-    return Promise.resolve(client.downloadsimages().withId("123").withFileType("jpg").execute()).then(res => {
-        t.is(res.response, "response");
-    });
+    const res = await client.downloadsimages().withId("123").withFileType("jpg").execute();
+    t.is(res.response, "response");
 });
 
-test("DownloadsImages: When given a height, the height will be part of the query", t => {  
+test("DownloadsImages: When given a height, the height will be part of the query", async t => {  
     var client = new api({ apiKey: "apikey", apiSecret: "apisecret" }, null);
-    return Promise.resolve(client.downloadsimages().withId("123").withHeight("592").execute()).then(res => {
-        t.is(res.response, "response");
-    });
+    const res = await client.downloadsimages().withId("123").withHeight("592").execute();
+    t.is(res.response, "response");
 });
 
-test("DownloadsImages: When given a product id, the product_id will be part of the query", t => {  
+test("DownloadsImages: When given a product id, the product_id will be part of the query", async t => {  
     var client = new api({ apiKey: "apikey", apiSecret: "apisecret" }, null);
-    return Promise.resolve(client.downloadsimages().withId("123").withProductId(5678).execute()).then(res => {
-        t.is(res.response, "response");
-    });
+    const res = await client.downloadsimages().withId("123").withProductId(5678).execute();
+    t.is(res.response, "response");
 });
 
-test("DownloadsImages: When given a product type, the product_type will be part of the query", t => {  
+test("DownloadsImages: When given a product type, the product_type will be part of the query", async t => {  
     var client = new api({ apiKey: "apikey", apiSecret: "apisecret" }, null);
-    return Promise.resolve(client.downloadsimages().withId("123").withProductType("easyaccess").execute()).then(res => {
-        t.is(res.response, "response");
-    });
+    const res = await client.downloadsimages().withId("123").withProductType("easyaccess").execute();
+    t.is(res.response, "response");
 });
 
